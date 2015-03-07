66a11e06f5a232cca2db24f12ad7de81779db5e4
Simone <quinotec@server20.websitehostserver.net>
2015-03-07 14:45:17 -0500

    First commit--everybody together

---
+++ js/editor.js
@@ -0,0 +1,159 @@
+/* Quinote Software Group 2015
+ *
+ * Author(s): Cameron Basham, Elliott Warkus
+ *
+ *
+ *
+ * TODO:
+ *    • Find meaningful way to handle user's text formatting
+ *    • Take more efficient approach to rebuilding list on text input
+ *    • Build more logical and aesthetically pleasing list
+ *    • Adapt handlers for new UI (and subsequently uncomment them)
+ *    • Get font stuff working
+ *      – Use selected font from get-go
+ *      – Keep it on "enter" input
+ *
+ */
+
+var editor;
+
+var main = function() {
+    // Initialize Quill editor
+    editor = new Quill('#editor', {
+        modules: {
+            'toolbar': {
+                container: '#toolbar'
+            }
+        },
+        styles: false
+    });
+
+    /*************************************
+     * Event Handlers
+     *************************************/
+    editor
+        .on('text-change', function(delta, source) {
+            buildList(parseEditorText());
+        })
+    ;
+
+    // Must use JQuery here to grab actual html object
+    $('#editor')
+        .click(function() {
+            editor.focus();
+        })
+    ;
+
+    //$('#fontsize')
+    //    .on('change')(function() {
+    //        $('.ql-size').add
+    //    })
+
+    //$(window).resize(resizeEditor);
+
+
+    // Set up initial environment
+    //resizeEditor();
+    editor.focus();
+    //$.each([8, 10, 12, 14, 16, 18, 24, 30, 36, 48, 72, 96], function(index, value) {
+    //    var fSize = $('.ql-size');
+    //    fSize.append($('<option value=' + this.value + '>' + value + '</option>'));
+    //            //.val(value + "px")
+    //            //.label(value));
+    //});
+    //fSize.val('12px').prop('selected', true);
+    //fSize.change();
+
+};
+
+$(document).ready(main);
+
+/*************************************
+ * Helper functions
+ *************************************/
+
+var resizeEditor = function() {
+    /* Resizes the editor box based on window height,
+     * top margin size, and toolbar height.
+     *
+     */
+    var spaceLeft = $(window).height()
+        - ($('.editor-wrapper').height() - $('#editor').height())
+        - (2 * parseInt($('#container-fluid').css('margin-top')))
+        - 2;
+    $('#editor').height(spaceLeft);
+    console.log($('#editor').height());
+};
+
+
+
+
+var parseEditorText = function() {
+    /* Parses the text inside of the editor
+     * after first formatting it appropriately.
+     *
+     * TODO:
+     *    • Parse results from getHTML() rather that getText()
+     */
+    var textArray = editor.getText().split("\n");
+    return parseInput(textArray);
+};
+
+var classString = function(element) {
+    if (element instanceof DateElement) {
+        return "date-element";
+    } else if (element.definitions.length > 0) {
+        return "identifier-element";
+    } else {
+        return "unknown-element";
+    }
+};
+
+var buildList = function(parseResult) {
+    /* Takes in a parse result, and rebuilds the
+     * #notes-list ordered list with it.
+     *
+     * TODO:
+     *    • Make this not so horribly inefficient
+     *    • Figure out how we should separate/format the types of keys
+     */
+    list = $('#notes-list ol');
+    list.empty();
+
+    for (i=0; i<parseResult.parsedElements.length; i++) {
+        var nextElement = parseResult.parsedElements[i];
+
+        var listItem = $('<li>').text(nextElement.getIdentifier());
+        listItem.addClass(classString(nextElement));
+        list.append(listItem);
+
+        if (nextElement.subelements.length > 0) {
+            // call recursive function on any subelements
+            list.append(buildSublist(nextElement.subelements, 1));
+        }
+    }
+};
+
+function buildSublist(elements, indentLevel) {
+    // recursively build sublists within the sidebar to reflect tab organization
+
+    var newList = "<ol>";
+
+    // create string of indents of length == indentLevel
+    var indents = "";
+    for (var i=0; i<indentLevel; i++) {
+        indents += "\t";
+    }
+
+    for (i=0; i<elements.length; i++) {
+        var nextElement = elements[i];
+        newList += indents + "<li class=" + classString(nextElement) + ">" + nextElement.getIdentifier() + "<\li>";
+        if (nextElement.subelements.length > 0) {
+            // recurse on sublists
+            newList += buildSublist(nextElement.subelements, indentLevel+1);
+        }
+    }
+    newList += "</ol>";
+
+    return newList;
+}
---
+++ js/frontend.js
@@ -0,0 +1,353 @@
+// A model for quiz code
+
+//**************************************
+// GLOBAL VARIABLES
+//**************************************
+
+var parseResult;
+var optionList;
+var quiz;
+var totalQuestions;
+var numberCorrect;
+var currentQuestion;
+var choices;
+var selectTF = ["true", "false"];
+var flag = true;
+
+//**************************************
+// HANDLERS
+//**************************************
+
+$(document).ready(function() {
+	$('#buttonGenerateQuiz').click(function() {// GENERATE QUIZ
+		
+		parseResult = parseInput(editor.getText().split("\n"));
+		optionList = new OptionList(2,2,2);
+		quiz = makeQuiz(parseResult, optionList);
+		console.log(parseResult, quiz);
+		totalQuestions = quiz.size();
+		numberCorrect = 0;
+		
+		$('#quizframe').toggle();
+		$('#scorepage').toggle();
+		$('#quizDialog').css("z-index", "4");
+		//showQuiz();
+	});
+	
+	$('#buttonCloseQuiz').click(function(){
+		$('#quizDialog').css("z-index", "-10");
+	});
+
+	$('#buttonStartQuiz').click(function(){
+		$('#quizframe').toggle();
+
+		nextQuestion();
+		flag = false;
+	});
+	
+	
+	$("#buttonNext").click(function() { // NEXT button
+		nextHandler();
+		
+	});
+
+	$("#buttonCheck").click(function() { // CHECK answer button
+
+	});
+});
+
+//**************************************
+// FUNCTIONS
+//**************************************
+
+function checkAnswer() {
+	
+	document.getElementById("checkCon").style.visibility="visible";
+	document.getElementById("answerSelections").style.visibility="hidden";
+	
+	
+	if (currentQuestion instanceof MultipleChoiceQuestion) {
+		checkMCQuestion();
+	} else if (currentQuestion instanceof TrueFalseQuestion) {
+		checkTFQuestion();
+	} else if (currentQuestion instanceof FillInTheBlankQuestion) {
+		checkFITBQuestion();
+	}
+	flag = true;
+}
+
+function checkMCQuestion() {
+	var inputs = $("[name='rd']");
+	var index = -1;
+	for (i=0; i<inputs.length; i++) {
+		if (inputs[i].checked) {
+			index = i;
+		}
+	}
+	if (index === -1) {
+        document.getElementById("answerSelections").style.visibility="visible";
+		alert("Please select an answer");
+		console.log("No answers selected", ($('input[name="rd"]:checked').length > 0));
+	} else {
+		var selected = choices[index];
+		console.log(selected);
+		if (selected === currentQuestion.answer) {
+			showCorrect();
+			numberCorrect++;
+		} else {
+			showIncorrect();
+		}
+		inputs[index].checked = false;
+		checkPage();
+	}
+}
+
+function checkTFQuestion() {
+	var inputs = $("[name='ch']");
+	var index = -1;
+	for (i=0; i<inputs.length; i++) {
+		if (inputs[i].checked) {
+			index = i;
+		}
+	}
+	if (index === -1) {
+        document.getElementById("answerSelections").style.visibility="visible";
+		alert("Please select an answer");
+		console.log("No answers selected", ($('input[name="rd"]:checked').length > 0));
+	} else {
+		var selected = selectTF[index];
+		if (selected === currentQuestion.answer) {
+			showCorrect();
+			numberCorrect++;
+		} else {
+			showIncorrect();
+		}
+		inputs[index].checked = false;
+		checkPage();
+	}
+}
+
+function checkFITBQuestion() {
+	var answerField = $("#ansText");
+	var input = $("#ansText").val();
+	if (input === "") {
+        document.getElementById("answerSelections").style.visibility="visible";
+		alert("Please enter an answer.");
+	} else {
+		if (input === currentQuestion.answer) {
+			showCorrect();
+			numberCorrect++;
+		} else {
+			showIncorrect();
+		}
+		answerField.val("");
+		checkPage();
+	}
+}
+		
+
+function nextHandler() {
+	// decide what to do upon pressing the 'next' button
+	if (quiz.hasNext()) {
+		if (flag) {
+			nextQuestion();
+			flag = false;
+		} 
+	} else {
+		tearDown(); //?
+	}
+}
+
+function nextQuestion() {
+	
+	document.getElementById("answerSelections").style.visibility="";
+	document.getElementById("buttonCheck").style.display="";
+	document.getElementById("buttonNext").style.display="none";
+	hideIncorrect();
+	hideCorrect();
+	
+	//global?
+	currentQuestion = quiz.getNext();
+	if (currentQuestion instanceof MultipleChoiceQuestion) {
+		initializeMCQuestion(currentQuestion);
+	} else if (currentQuestion instanceof TrueFalseQuestion) {
+		initializeTFQuestion(currentQuestion);
+	} else if (currentQuestion instanceof FillInTheBlankQuestion) {
+		initializeFITBQuestion(currentQuestion);
+	} else {
+		console.log("Error: Question type unrecognized.");
+	}
+}
+	
+
+function initializeMCQuestion(question) {
+	choices = currentQuestion.getAllAnswers();
+	var questionTag = $("#question");
+	$(questionTag).html(question.getText());
+	hideText();
+	showRadioChoice();
+	showRadiobuttons(question);
+	hideCheck();
+	
+	// not sure what this does?
+	$("#answer_choice").insertAfter("#formAction");
+}
+
+function initializeTFQuestion(question) {
+	var questionTag = $("#question");
+	$(questionTag).html(question.getText());
+	hideChoice();
+	hideText();
+	showCheckbuttons();
+	showCheck();
+	
+	$("#answer_check").insertAfter("#formAction");
+}
+
+function initializeFITBQuestion(question) {
+	var questionTag = $("#question");
+	$(questionTag).html(question.getText());
+	hideChoice();
+	showText();
+	hideCheck();
+	$("#answer_text").insertAfter("#formAction");
+}
+	
+
+function endQuiz(numberCorrect) {
+	// finish the quiz, present the percentage correct, etc.
+}
+
+function tearDown() {
+	// hide DOM elements, do necessary cleanup
+	completeQuiz();
+}
+
+
+///// COPIED FUNCTIONS /////
+
+function showQuiz(){
+	document.getElementById("quizopener").style.display="none";
+	document.getElementById("quizframe").style.visibility="visible";
+	document.getElementById("scorepage").style.display="none";
+	document.getElementById("svgOpener").style.display="none";
+	document.getElementById("scoreKeeper").style.display="none";
+	document.getElementById("one").style.display="none";
+	document.getElementById("two").style.display="none";
+};
+
+function checkPage(){
+	document.getElementById("buttonCheck").style.display="none";
+	document.getElementById("buttonNext").style.display="";
+}
+function showCheck(){
+	document.getElementById("answer_check").style.display="";
+}
+function hideCheck(){
+	document.getElementById("answer_check").style.display="none";
+}
+function showText(){
+	document.getElementById("answer_text").style.display="";
+}
+function hideText(){
+	document.getElementById("answer_text").style.display="none";
+}
+function showRadiobuttons(question){
+	//var choices = question.getAllAnswers();
+	
+	document.getElementById("rad1").innerHTML = choices[0];
+	document.getElementById("rad2").innerHTML = choices[1];
+	document.getElementById("rad3").innerHTML = choices[2];
+	document.getElementById("rad4").innerHTML = choices[3];
+	document.getElementById("rad1").value = choices[0];
+	document.getElementById("rad2").value = choices[1];
+	document.getElementById("rad3").value = choices[2];
+	document.getElementById("rad4").value = choices[3];
+}
+function showCheckbuttons(){
+	document.getElementById("true").innerHTML = selectTF[0];
+	document.getElementById("false").innerHT = selectTF[1];
+	document.getElementById("true").value = selectTF[0];
+	document.getElementById("false").value = selectTF[1];
+	return;
+}
+function showRadioChoice(){
+	document.getElementById("answer_choice").style.display="";
+}
+function hideChoice(){
+	document.getElementById("answer_choice").style.display="none";
+}
+function hideCorrect(){
+	document.getElementById("one").style.display="none";
+}
+function hideIncorrect(){
+	document.getElementById("two").style.display="none";
+}
+function showCorrect(){
+	document.getElementById("one").style.display="";
+	$('#one').toggle;
+	var oneAns=document.getElementById("one");
+	var aText=" is correct!";
+	oneAns.innerHTML= currentQuestion.answer + aText;
+	document.getElementById("buttonNext").style.display="";
+}
+function showIncorrect(){
+	document.getElementById("two").style.display="";
+	$('#two').toggle;
+	var twoAns=document.getElementById("two");
+	var a2Text="Incorrect! The answer is: ";
+	twoAns.innerHTML= a2Text + currentQuestion.answer;
+	document.getElementById("buttonNext").style.display="";	
+}
+
+var dialog = (function() {
+	function init() {
+		var overlay = document.querySelector( '.dialogShadow' );
+		[].slice.call( document.querySelectorAll( '.dialogTrigger' ) ).forEach( function( el, i ) {
+			var transform = document.querySelector( '#quizDialog' ),
+			close = transform.querySelector( '.dialogClose' );
+			function removedialog() {
+				$("#quizDialog").removeClass("dialogVisible");
+			}
+			function removedialogHandler() {
+				removedialog();
+			}
+			el.addEventListener( 'click', function( ev ) {
+				$("#quizDialog").addClass("dialogVisible");
+			});
+			close.addEventListener( 'click', function( ev ) {
+				removedialogHandler();
+			});
+		});
+	}
+	init();
+})();
+
+function randomizeQuiz(){};
+
+function completeQuiz(){
+	var score_exit;
+	document.getElementById("scr").value = score_exit;
+	var om = document.getElementById("qLength").value;
+	var tot = (parseInt(om));
+	$("#question").html("<h4>Your score is: "+numberCorrect+"/"+totalQuestions+"<h4>");
+	$("#scoreDisplay").html("" +numberCorrect+"/"+totalQuestions+ "");
+	document.getElementById("answer_text").style.display="none";
+	document.getElementById("answer_choice").style.display="none";
+	document.getElementById("answer_check").style.display="none";
+	document.getElementById("scorepage").style.display="";
+	document.getElementById("buttonNext").style.display="none";
+	document.getElementById("question").style.display="none";
+	document.getElementById("buttonCheck").style.display="none";
+	document.getElementById("quizframe").style.display="none";
+	document.getElementById("quizopener").style.display="none";
+};
+
+
+$(function(){
+	$("#editorspace").draggable({
+		handle: "#editorToolbar",
+		containment: "parent"
+	});
+	$("#sidebar").draggable({ containment: "parent" });
+});
