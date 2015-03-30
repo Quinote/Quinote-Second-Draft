dcafba192bc3698c8cfd5741685efbc3a6b2b275
Simone <quinotec@server20.websitehostserver.net>
2015-03-29 23:12:35 -0400

    Files can save

---
+++ trylogin/save.php
@@ -0,0 +1,21 @@
+<?php
+
+require('methods.php');
+
+//make sure logged in user id = Post user id
+$checkid = $_SESSION['userid'];
+$userid = mysql_real_escape_string($_POST['userid']);
+if(!islogged() || $userid != $checkid) {
+	header('Location: index.php');
+}
+
+
+else {
+$fileid = mysql_real_escape_string($_POST['fileid']);
+$content = mysql_real_escape_string($_POST['content']);
+
+$res = mysql_query("UPDATE `files` SET `content`='$content' WHERE `file_id`='$fileid' AND `owner_id`='$userid'");
+echo $res;
+}
+
+?>
